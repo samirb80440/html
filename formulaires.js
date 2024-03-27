fe97f7a218e83a1371ba677bb72ea6d660cf4293
Samir <samirb80440@gmail.com>
2024-03-27 15:08:43 +0100

    modif

--- formulaires.js
+++ formulaires.js
@@ -1,35 +1,35 @@
 function verif2() {
-  Coordonées = document.getElementById("Société");
-  Filtre = new RegExp("+[A-Za-z]");
-  FitreCode = new RegExp("^[0-9]{5}*$");
-  FitreEmail = new RegExp("^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$");
+  Coordonées = document.getElementById("Société").value
+  Filtre = new RegExp("^[A-Za-z]")
+  FiltreCode = new RegExp("^([0-9]{5})*$")
+  FiltreEmail = new RegExp("^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$")
   // Filtretel=new RegExp("^0[1-9]([-. ]?\d{2}){4}$")
-  resultat = Filtre.test(Coordonées);
+  resultat = Filtre.test(Coordonées)
   if (Coordonées == false) {
     alert("Entrez le nom de la société");
   }
-  Coordonées = document.getElementById("Personne");
-  resultat = Filtre.test(Coordonées);
+  Coordonées = document.getElementById("Personne").value
+  resultat = Filtre.test(Coordonées)
   if (Coordonées == false) {
-    alert("Entrez le nom de la Personne à contacter");
+    alert("Entrez le nom de la Personne à contacter")
   }
-  Coordonées = document.getElementById("Codepostal");
-  resultat2 = FiltreCode.test(Coordonées);
+  Coordonées = document.getElementById("Codepostal").value
+  resultat2 = FiltreCode.test(Coordonées)
   if (resultat2 == false) {
-    alert("Entrez le code postal");
+    alert("Entrez le code postal")
   }
-  Coordonées = document.getElementById("Ville");
-  resultat = Filtre.test(Coordonées);
+  Coordonées = document.getElementById("Ville").value
+  resultat = Filtre.test(Coordonées)
   if (Coordonées == false) {
     alert("Entrez la ville");
   }
-  Coordonées = document.getElementById("Email");
-  resultat3 = FiltreEmail.test(Coordonées);
+  Coordonées = document.getElementById("Email").value
+  resultat3 = FiltreEmail.test(Coordonées)
   if (resultat3 == false) {
     alert("Entrez un Email");
   }
 }
-const re = document.getElementById("Choissis");
+const re = document.getElementById("Choissis")
 const choixElement = document.getElementById("Choissez");
 const textareaElement = document.getElementById("Sélectionnez");
 
