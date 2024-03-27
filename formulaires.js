a98ce233a2f9e35396ce7e8eec06c9f182f047df
Samir <samirb80440@gmail.com>
2024-03-27 11:37:11 +0100

    samirdif

--- formulaires.js
+++ formulaires.js
@@ -1,58 +1,44 @@
-function verif2(){
- Coordonées= document.getElementById("Société")
- Filtre = new RegExp( "+[A-Za-z]")
- FitreCode= new RegExp("^[0-9]{5}*$")
- FitreEmail= new RegExp("^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$")
- // Filtretel=new RegExp("^0[1-9]([-. ]?\d{2}){4}$")
-resultat= Filtre.test(Coordonées);
-if(Coordonées ==false){
-    alert("Entrez le nom de la société")
+function verif2() {
+  Coordonées = document.getElementById("Société");
+  Filtre = new RegExp("+[A-Za-z]");
+  FitreCode = new RegExp("^[0-9]{5}*$");
+  FitreEmail = new RegExp("^[a-z0-9.-]+@[a-z0-9.-]{2,}.[a-z]{2,4}$");
+  // Filtretel=new RegExp("^0[1-9]([-. ]?\d{2}){4}$")
+  resultat = Filtre.test(Coordonées);
+  if (Coordonées == false) {
+    alert("Entrez le nom de la société");
+  }
+  Coordonées = document.getElementById("Personne");
+  resultat = Filtre.test(Coordonées);
+  if (Coordonées == false) {
+    alert("Entrez le nom de la Personne à contacter");
+  }
+  Coordonées = document.getElementById("Codepostal");
+  resultat2 = FiltreCode.test(Coordonées);
+  if (resultat2 == false) {
+    alert("Entrez le code postal");
+  }
+  Coordonées = document.getElementById("Ville");
+  resultat = Filtre.test(Coordonées);
+  if (Coordonées == false) {
+    alert("Entrez la ville");
+  }
+  Coordonées = document.getElementById("Email");
+  resultat3 = FiltreEmail.test(Coordonées);
+  if (resultat3 == false) {
+    alert("Entrez un Email");
+  }
 }
-Coordonées=document.getElementById("Personne")
-resultat=Filtre.test(Coordonées);
-if(Coordonées ==false){
-    alert("Entrez le nom de la Personne à contacter")
-}
-Coordonées=document.getElementById("Codepostal")
-resultat2=FiltreCode.test(Coordonées)
-if(resultat2 ==false){
-    alert("Entrez le code postal")
-}
-Coordonées=document.getElementById("Ville")
-resultat=Filtre.test(Coordonées)
-if(Coordonées ==false){
-    alert("Entrez la ville")
+const re = document.getElementById("Choissis");
+const choixElement = document.getElementById("Choissez");
+const textareaElement = document.getElementById("Sélectionnez");
 
-}
-Coordonées=document.getElementById("Email")
-resultat3=FiltreEmail.test(Coordonées)
-if(resultat3==false){
-    alert("Entrez un Email")
-}
-}
-const choixElement= document.getElementById('Choisssez')
-const textareaElement=document.getElementById('Sélectionnez')
-
-/*choixElement.addEventListener('change',() => {
-const choix=choixElement.value;
-if (choix!== "choix0") {
-    textareaElement.style.dislplay ="block";
-} else {
-    textareaElement.style.display ="none";
-}
-
+choixElement.addEventListener("change", () => {
+  const choix = choixElement.value;
+  if (choix !== "Choissis") {
+    textareaElement.style.dislplay = "block";
+    re.remove("Choissis");
+  } else {
+    textareaElement.style.display = "none";
+  }
 });
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
